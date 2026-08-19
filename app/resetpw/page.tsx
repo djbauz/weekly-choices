05a35d46152d7fb031ba520754ea1a23be466eff
Matteo Bante <djbauz@gmail.com>
2026-08-19 23:01:32 +0200

    cambio minore resetpw

--- app/resetpw/page.tsx
+++ app/resetpw/page.tsx
@@ -14,22 +14,14 @@ export default function MyApp() {
     const email = searchParams.get("email");
     console.log(email);
 
-    //const [email,setEmail]=useState("")
     const [new_password,setPassword]=useState("")
     const [confirmPw, setConfirmPW]=useState("")
-    //const [nickname,setNickname]=useState("")
 
-    const signup = async (e: SubmitEvent<HTMLFormElement>) => {
+    const resetpwd = async (e: SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
         
         console.log("Email:", email);
 
-        //const emailValue = email.trim().toLowerCase();
-        //const nicknameValue = nickname.trim();
-
-        //if (!emailValue) return alert("Email is required");
-        //if (!emailValue.includes("@")) return alert("Enter a valid email");
-        //if (!nicknameValue) return alert("Nickname is required");
         if (new_password.length < 8 || new_password.length > 20) return alert("Password must be between 8 and 20 characters long");
         if (new_password !== confirmPw) return alert("Passwords don't match");
 
@@ -54,11 +46,7 @@ export default function MyApp() {
 
             <div className="container">
                 <div className="card">
-                <form onSubmit={signup}>
-                    {/* <input className="contentSignup" type="text" autoComplete="nickname" placeholder="nickname" onChange={e=>setNickname(e.target.value)} />
-                    <br />
-                    <input className="contentSignup" type="email" autoComplete="email" placeholder="email" onChange={e=>setEmail(e.target.value)} />
-                    <br /> */}
+                <form onSubmit={resetpwd}>
                     <input className="contentSignup" type="password" autoComplete="new-password" placeholder="password" onChange={e=>setPassword(e.target.value)} />
                     <br />
                     <input className="contentSignup" type="password" autoComplete="new-password" placeholder="Retype password" onChange={e=>setConfirmPW(e.target.value)} />
@@ -71,11 +59,3 @@ export default function MyApp() {
         </>
     );
 }
-
-/* ISTRUZIONI:
-- Seleziona il tuo nickname preferito, la tua mail e la password. 
-- La password deve avere tra gli 8 e i 20 caratteri
-- Una volta completati i dati, fai click su SIGNUP
-- Attendi una mail all'indirizzo indicato e poi fai click sul link ricevuto per attivare l'account
-- 
-*/
