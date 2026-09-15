9600afd4011827f9a0cc763d83a2867ab426b624
Matteo Bante <djbauz@gmail.com>
2026-09-15 20:19:41 +0200

    test bottone inizio lega v4

--- app/league-admin/[id]/page.tsx
+++ app/league-admin/[id]/page.tsx
@@ -800,6 +800,7 @@ export default function LeagueAdminPage() {
         )}
         <br />
       </div>
+      <br />
 
         {/* ------------------------------------------------------
             Open League
